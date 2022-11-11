37ec480eadf044cc5da030680b77ffa974a63141
Huichan <zgmlcks53@gmail.com>
2022-11-11 12:17:45 +0900

    test

--- Eyediagram/main/run.py
+++ Eyediagram/main/run.py
@@ -3,4 +3,5 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from src import Eye
 node = 'n77'
+SampleName = 'sample'
 Eye.Eye(50,2,-2,0,500,25,0.176e-9,26e-15,1.31,node)
